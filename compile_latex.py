7292e505b18c89c3cf21bb9d4bafd2a7e733b248
luGil2433 <luciagil@bu.edu>
2025-04-27 18:48:53 -0400

    Now it does not depend on user to allow latext errors

--- compile_latex.py
+++ compile_latex.py
@@ -4,7 +4,7 @@ import os
 def compile_latex_to_pdf(tex_filename):
     try:
         subprocess.run(
-            ["pdflatex", "-output-directory=outputs", tex_filename],
+            ["pdflatex", "-output-directory=outputs", "-interaction=nonstopmode", tex_filename],
             check=True
         )
         print(f"PDF created successfully for {tex_filename}")
